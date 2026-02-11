32bcbf492f738aab4df64e734b699139c4151d5b
Abhishek <136466988+Abhishe58@users.noreply.github.com>
2026-02-11 07:33:39 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+# Load model once
+model = joblib.load("xcod.pkl")
+features = joblib.load("model_features.pkl")
+
+app = FastAPI(title="Phone Addiction Predictor")
+
+class UserInput(BaseModel):
+    Age: int
+    Gender: int
+    School_Grade: int
+    Daily_Usage_Hours: float
+    Sleep_Hours: float
+    Academic_Performance: int
+    Social_Interactions: int
+    Exercise_Hours: float
+    Anxiety_Level: int
+    Depression_Level: int
+    Self_Esteem: int
+    Parental_Control: int
+    Screen_Time_Before_Bed: float
+    Phone_Checks_Per_Day: int
+    Apps_Used_Daily: int
+    Time_on_Social_Media: float
+    Time_on_Gaming: float
+    Time_on_Education: float
+    Phone_Usage_Purpose: int
+    Family_Communication: int
+    Weekend_Usage_Hours: float
+
+def addiction_label(score: float):
+    if score < 4:
+        return "Low"
+    elif score < 7:
+        return "Moderate"
+    else:
+        return "High"
+
+@app.post("/predict")
+def predict(data: UserInput):
+    df = pd.DataFrame([[getattr(data, f) for f in features]], columns=features)
+
+    score = float(model.predict(df)[0])
+    label = addiction_label(score)
+
+    return {
+        "addiction_score": round(score, 2),
+        "addiction_level": label
+    }
